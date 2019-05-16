9798d72b8da051682a67d5c0b61bfbac72ca96b9
Chris Sinjakli <chris@gocardless.com>
2019-05-16 16:37:45 +0100

    Implement find-chain command in CLI

--- lib/coach/cli/provider_finder.rb
+++ lib/coach/cli/provider_finder.rb
@@ -10,7 +10,7 @@ module Coach
         @value_name = value_name
       end
 
-      def run
+      def find_provider
         if Module.const_defined?(@middleware_name)
           middleware = Module.const_get(@middleware_name)
         else
@@ -25,9 +25,40 @@ module Coach
           raise "That value isn't provided!"
         end
 
-        puts "Value `#{@value_name}` is provided by:\n\n"
+        puts "Value `#{@value_name}` is provided to `#{@middleware_name}` by:\n\n"
         puts providers.to_a.join("\n")
       end
+
+      def find_chain
+        if Module.const_defined?(@middleware_name)
+          middleware = Module.const_get(@middleware_name)
+        else
+          raise "That middleware doesn't exist!"
+        end
+
+        provider_chain = build_provider_chain(middleware, Hash.new { Set.new }, [])
+
+        if provider_chain.key?(@value_name.to_sym)
+          chains = provider_chain[@value_name.to_sym]
+        else
+          raise "That value isn't provided!"
+        end
+
+        if chains.size > 1
+          puts "Value `#{@value_name}` is provided to `#{@middleware_name}` " \
+            "by multiple middleware chains:\n\n"
+        else
+          puts "Value `#{@value_name}` is provided to `#{@middleware_name}` by:\n\n"
+        end
+
+        formatted_chains = chains.map do |chain|
+          chain.join(" -> ")
+        end.join("\n---\n")
+
+        puts formatted_chains
+      end
+
+      private
 
       def build_provider_mapping(middleware, mapping)
         middleware.provided.each do |p|
@@ -36,6 +67,20 @@ module Coach
 
         middleware.middleware_dependencies.each do |dep|
           build_provider_mapping(dep.middleware, mapping)
+        end
+
+        mapping
+      end
+
+      def build_provider_chain(middleware, mapping, chain)
+        new_chain = chain + [middleware]
+
+        middleware.provided.each do |p|
+          mapping[p] = mapping[p].add(new_chain)
+        end
+
+        middleware.middleware_dependencies.each do |dep|
+          build_provider_chain(dep.middleware, mapping, new_chain)
         end
 
         mapping
